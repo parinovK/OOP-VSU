df6f11b34bf9087e7285f686f95906596a0eaa13
parinovK <62334658+parinovK@users.noreply.github.com>
2022-08-25 19:13:20 +0300

    add project
    add .cpp and .h files

---
+++ LabWork1/Header.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+#ifndef Header_h
+#define Header_h
+
+class int_digit
+{
+public:
+	int_digit()
+	{
+		a = 0;
+		b = 0;
+		c = 0;
+		d = 0;
+	}
+	
+	int_digit(int a, int b, int c, int d)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+		this->d = d;
+	}
+
+	double Funct()
+	{
+		double sum = 0;
+		sum = (a + b + c + d)/4;
+		return sum;
+	}
+
+	int max_digit()
+	{
+		int max;
+		if ((a > b) && (a > c))
+			max = a;
+		else
+		{
+			if (b > c)
+				max = b;
+			else
+				max = c;
+		}
+		if (d > max)
+			max = d;
+		return max;
+	}
+
+	~int_digit()
+	{
+		std::cout << "Изничтожено";
+	};
+
+private:
+	int a;
+	int b;
+	int c;
+	int d;
+};
+#endif /* Vector_h */
---
+++ LabWork1/Laba1.cpp
@@ -0,0 +1,22 @@
+﻿// Laba1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+#include "Header.h"
+#include <cmath>
+
+int main()
+{
+	int a, b, c, d, max; //дано четыре целых числа
+	double sum; //для вычисления среднего арифметического
+
+	std::cout << "enter four digits with space: " << std::endl;
+	std::cin >> a >> b >> c >> d;
+
+	int_digit Digits(a,b,c,d);
+	sum = Digits.Funct();
+	max = Digits.max_digit();
+
+	std::cout << "arithmetic mean = " << sum << std::endl << "Max digit = " << max << std::endl;
+	return 0;
+}
+
